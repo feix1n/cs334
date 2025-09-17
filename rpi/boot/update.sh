f34daa44445af24acd1d7a1c0ddd4983684e7401
feix1n <cindy.mo@yale.edu>
2025-09-17 10:38:48 -0400

    update IP address

--- rpi/boot/update.sh
+++ rpi/boot/update.sh
@@ -10,10 +10,11 @@ DATE=$(date)
 echo "$DATE IP Address is $IP" >> ~/cs334/rpi/boot/ip.md
 # what's the actual file name lol & get the ip thru grep?????
 echo "$IP" >> ~/cs334/rpi/boot/ip.md
+echo "1" >> ~/cs334/rpi/boot/ip.md
 
 # copy the config files over
 cp -r ~/.config configcopy
 
 git add .
 git commit -m "update IP address"
-git push
+git push origin
