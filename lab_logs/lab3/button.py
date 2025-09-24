8b25874ebb6543cdc593fd4f9f5950dd08a98e29
feix1n <cindy.mo@yale.edu>
2025-09-23 20:26:09 -0400

    Added lab3 material

---
+++ lab_logs/lab3/button.py
@@ -0,0 +1,19 @@
+from gpiozero import Button
+
+button = Button(14)
+switch = Button(15)
+joysticksw = Button(18)
+
+while True:
+    if button.is_pressed:
+        print("Button is pressed", end = " | ")
+    else:
+        print("Button is not pressed", end = " | ")
+    if switch.is_pressed:
+    	print("Switch on", end = " | ")
+    else:
+    	print("Switch off", end = " | ")
+    if joysticksw.is_pressed:
+    	print("Joystick clicked")
+    else:
+    	print("Joystick not clicked")
